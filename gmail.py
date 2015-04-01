df05ee8561d64426069d49558e3006e03fbdceb9
Ali Razmjoo <anti.securityboy@gmail.com>
2015-04-01 13:09:36 +0430

    remove lines which is not usable

--- gmail.py
+++ gmail.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python2.7
-import smtplib,email,os,sys
-from email.MIMEMultipart import MIMEMultipart
-from email.Utils import COMMASPACE
-from email.MIMEText import MIMEText
-#from email.MIMEBase import MIMEBase
-#from email.parser import Parser
-#from email.MIMEImage import MIMEImage
-#from email.MIMEAudio import MIMEAudio
-#import mimetypes
-import email
-import email.mime.application
+import smtplib
 user = 'xxx@gmail.com'
 passw = 'xxx'
 smtp_host = 'smtp.gmail.com'
@@ -18,4 +8,4 @@ server = smtplib.SMTP()
 server.connect(smtp_host,smtp_port)
 server.ehlo()
 server.starttls()
-a = server.login(user,passw)
+server.login(user,passw)
